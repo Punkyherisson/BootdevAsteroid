e03243807f696768197a65581153ac711fcbb276
Punkyherisson <emmanuel.goitia@gmail.com>
2025-01-15 10:05:01 +0100

    Adding Player and showing it as a triangle in the window

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ import pygame
 # Import constants from constants.py
 from constants import *
 
+#Import the Player Class (sprite step)
+from player import Player
+
 def main():
     # Initialize pygame
     pygame.init()
 
     # Set up the screen with the defined width and height
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #Create a Player Instance
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Create a Clock object to manage FPS
+    clock = pygame.time.Clock()
 
     # Game loop
     running = True
@@ -21,9 +29,14 @@ def main():
 
         # Fill the screen with a solid black color
         screen.fill((0, 0, 0))
-
+        
+        # Render the Player in the Game Loop
+        player.draw(screen)
         # Refresh the screen (update the display)
         pygame.display.flip()
+
+        # Limit FPS to 60
+        dt = clock.tick(60) / 1000  # delta time in seconds
 
     # Quit pygame when the game loop ends
     pygame.quit()
